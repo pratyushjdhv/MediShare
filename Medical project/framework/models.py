a714e32f778e2c593494897b157741a094e1f61d
pratyushjdhv <pratyushjdhv@gmail.com>
2024-05-04 02:31:12 +0530

    crazy ass update

--- Medical project/framework/models.py
+++ Medical project/framework/models.py
@@ -1,100 +1,173 @@
-from framework import db,bcrypt
+from framework import db, bcrypt
 from framework import login_manager
 from flask_login import UserMixin
 from sqlalchemy import ForeignKey
 
+
 class users(db.Model, UserMixin):
+    """
+    Represents a user in the system.
+
+    Attributes:
+        userid (int): The unique identifier for the user.
+        username (str): The username of the user.
+        fullname (str): The full name of the user.
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the user.
+        pincode (str): The pincode of the user.
+        pharmacy_name (str): The name of the pharmacy associated with the user.
+        pharmaid (int): The unique identifier for the pharmacy.
+        address (str): The address of the user.
+        points (int): The number of points the user has.
+
+    Methods:
+        password (property): Gets the hashed password of the user.
+        password (setter): Sets the hashed password of the user.
+        check_password: Checks if the provided password matches the user's hashed password.
+        get_id: Gets the string representation of the user's ID.
+    """
     userid = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    username = db.Column(db.String(35), nullable=False)  
+    username = db.Column(db.String(35), nullable=False)
     fullname = db.Column(db.String(35), nullable=False)
     email = db.Column(db.String(320), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(length=65), nullable=False)  
-    pincode=db.Column(db.String(6), default='000000')
+    hashed_password = db.Column(db.String(length=65), nullable=False)
+    pincode = db.Column(db.String(6), default='000000')
     pharmacy_name = db.Column(db.String(65))
-    pharmaid=db.Column(db.Integer,default=0)
+    pharmaid = db.Column(db.Integer, default=0)
     address = db.Column(db.String(256))
-    points = db.Column(db.Integer,default=0)
-    
+    points = db.Column(db.Integer, default=0)
+
     @property
     def password(self):
         return self.hashed_password
 
     @password.setter
     def password(self, plain_text_password):
-        self.hashed_password = bcrypt.generate_password_hash(plain_text_password).decode('utf8')
+        self.hashed_password = bcrypt.generate_password_hash(
+            plain_text_password).decode('utf8')
 
     def check_password(self, attempted_password):
         return bcrypt.check_password_hash(self.hashed_password, attempted_password)
 
     def get_id(self):
-        return str(self.userid) # Convert to string because Flask-Login expects a string ID
-    
+        # Convert to string because Flask-Login expects a string ID
+        return str(self.userid)
 
 
-    
+class pharmacy(db.Model, UserMixin):
+    """
+    Represents a pharmacy in the system.
 
-class pharmacy(db.Model,UserMixin):
+    Attributes:
+        pharmaid (int): The unique identifier for the pharmacy.
+        license_no (str): The license number of the pharmacy.
+        username (str): The username of the pharmacy.
+        pincode (int): The pincode of the pharmacy's location.
+        email (str): The email address of the pharmacy.
+        hashed_password (str): The hashed password of the pharmacy.
+        address (str): The address of the pharmacy.
+
+    Methods:
+        password (property): Gets the hashed password of the pharmacy.
+        password (setter): Sets the hashed password of the pharmacy.
+        check_password: Checks if the provided password matches the hashed password of the pharmacy.
+        get_id: Gets the string representation of the pharmacy's ID.
+    """
     pharmaid = db.Column(db.Integer, autoincrement=True, primary_key=True)
     license_no = db.Column(db.String(12))
-    username=db.Column(db.String(65))
+    username = db.Column(db.String(65))
     pincode = db.Column(db.Integer, nullable=False)
-    email=db.Column(db.String(320),nullable=False)
-    hashed_password=db.Column(db.String(length=65),nullable=False)
+    email = db.Column(db.String(320), nullable=False)
+    hashed_password = db.Column(db.String(length=65), nullable=False)
     address = db.Column(db.String(256), nullable=False)
 
     @property
     def password(self):
         return self.hashed_password
-    
+
     @password.setter
     def password(self, plain_text_password):
-        self.hashed_password = bcrypt.generate_password_hash(plain_text_password).decode('utf8')
-        
+        self.hashed_password = bcrypt.generate_password_hash(
+            plain_text_password).decode('utf8')
+
     def check_password(self, attempted_password):
         return bcrypt.check_password_hash(self.hashed_password, attempted_password)
-    
+
     def get_id(self):
-        return str(self.pharmaid) # Convert to string because Flask-Login expects a string ID
-    
+        # Convert to string because Flask-Login expects a string ID
+        return str(self.pharmaid)
 
 
+class medicine_request_pool(db.Model, UserMixin):
+    """
+    Represents a medicine request in the pool.
 
-'''class pharma_address(db.Model,UserMixin):
-    address_id=db.Column(db.Integer,autoincrement=True,primary_key=True)
-    license_no=db.Column(db.String(12),ForeignKey('pharmacy.license_no'))
-    state=db.Column(db.String(35))
-    district=db.Column(db.String(35))
-    city=db.Column(db.String(35))
-    pincode=db.Column(db.String(6))
-    street_address=db.Column(db.String(200))'''
-    
-class medicine_request_pool(db.Model,UserMixin):
-    id=db.Column(db.Integer,autoincrement=True,primary_key=True)
-    userid=db.Column(db.Integer)
-    prescription_name=db.Column(db.String(100))
-    pincode=db.Column(db.String(6), default='000000')
+    Attributes:
+        id (int): The unique identifier for the medicine request.
+        userid (int): The user ID associated with the medicine request.
+        prescription_name (str): The name of the prescription.
+        pincode (str): The pincode associated with the medicine request.
+        medications (str): The list of medications requested.
+        is_rejected (bool): Indicates if the request has been rejected.
+        is_approved (bool): Indicates if the request has been approved.
+        reason (str): The reason for rejection or unknown reason.
+        pharmaid (int): The ID of the pharmacy assigned to the request.
+        points_given (int): The number of points assigned for the request.
+    """
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    userid = db.Column(db.Integer)
+    prescription_name = db.Column(db.String(100))
+    pincode = db.Column(db.String(6), default='000000')
     medications = db.Column(db.String(2000))
-    is_rejected = db.Column(db.Boolean, default=False)  # Boolean value to indicate if the request is urgent
+    is_rejected = db.Column(db.Boolean, default=False)
     is_approved = db.Column(db.Boolean, default=False)
-    reason=db.Column(db.String(256),default="Not known")
-    pharmaid = db.Column(db.Integer,default=0)#this should be taken by the user 
-    points_given = db.Column(db.Integer, default=0)#are points assigned?
+    reason = db.Column(db.String(256), default="Not known")
+    pharmaid = db.Column(db.Integer, default=0)
+    points_given = db.Column(db.Integer, default=0)
+
 
 class meds_scheduler(db.Model):
+    """
+    Represents a medication scheduler.
+
+    Attributes:
+        id (int): The unique identifier for the medication scheduler.
+        med_name (str): The name of the medication.
+        dosage (str): The dosage of the medication.
+        frequency (str): The frequency at which the medication should be taken.
+        user_id (int): The ID of the user associated with the medication scheduler.
+        notes (str): Additional notes or comments about the medication.
+    """
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    med_name = db.Column(db.String(100),nullable=False)
+    med_name = db.Column(db.String(100), nullable=False)
     dosage = db.Column(db.String(50), nullable=False)
-    frequency = db.Column(db.String(50), nullable=False)    
-    user_id = db.Column(db.Integer, db.ForeignKey(users.userid), nullable=False)
+    frequency = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        users.userid), nullable=False)
     notes = db.Column(db.Text, nullable=True)
-    
+
+
 class donation_box(db.Model):
+    """
+    Represents a donation box in the medical project framework.
+
+    Attributes:
+        id (int): The unique identifier for the donation box.
+        equip_name (str): The name of the equipment in the donation box.
+        points_given (int): The number of points given for the donation box.
+        userid (int): The user ID associated with the donation box.
+        pincode (str): The pincode of the donation box.
+        is_rejected (bool): A boolean value indicating if the donation box is rejected.
+        is_approved (bool): A boolean value indicating if the donation box is approved.
+        reason (str): The reason for rejection or unknown if not specified.
+        pharmaid (int): The pharmacy ID associated with the donation box.
+    """
     id = db.Column(db.Integer, primary_key=True)
-    equip_name=db.Column(db.String(100))
+    equip_name = db.Column(db.String(100))
     points_given = db.Column(db.Integer, default=0)
-    userid=db.Column(db.Integer)
-    pincode=db.Column(db.String(6), default='000000')
-    is_rejected = db.Column(db.Boolean, default=False)  # Boolean value to indicate if the request is urgent
+    userid = db.Column(db.Integer)
+    pincode = db.Column(db.String(6), default='000000')
+    is_rejected = db.Column(db.Boolean, default=False)
     is_approved = db.Column(db.Boolean, default=False)
-    reason=db.Column(db.String(256),default="Not known")
-    pharmaid = db.Column(db.Integer,default=1)
+    reason = db.Column(db.String(256), default="Not known")
+    pharmaid = db.Column(db.Integer, default=1)
--- Medical project/framework/routes.py
+++ Medical project/framework/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from framework.forms import *
-from framework import app, db, models, login_manager, MED_UPLOAD_FOLDER, PRE_UPLOAD_FOLDER,EQU_UPLOAD_FOLDER,BILL_UPLOAD_FOLDER
+from framework import app, db, models, login_manager, MED_UPLOAD_FOLDER, PRE_UPLOAD_FOLDER, EQU_UPLOAD_FOLDER, BILL_UPLOAD_FOLDER
 from framework.models import *
 from flask_login import login_required, LoginManager, login_user, current_user, logout_user
 import os
@@ -14,18 +14,28 @@ def load_user(user_id):
     pharma_user = pharmacy.query.get(int(user_id))
     return pharma_user
 '''
+
 @app.route('/')
 @app.route('/home')
 def home():
-    if current_user.is_authenticated:
-        pharmacies=pharmacy.query.filter_by(pincode=current_user.pincode)
-        return render_template('home.html', user=current_user,pharmacies=pharmacies)
+    if current_user.is_authenticated and current_user.pincode:
+        pharmacies = pharmacy.query.filter_by(pincode=current_user.pincode)
+        return render_template('home.html', user=current_user, pharmacies=pharmacies)
     return render_template('home.html')
-    
 
-@app.route('/donate',methods=["POST","GET"])
+
+@app.route('/profile_page')
+def profile_page():
+    pharmacies = pharmacy.query.filter_by(pincode=current_user.pincode)
+    pharmacount = pharmacy.query.filter_by(
+        pincode=current_user.pincode).count()
+    return render_template('profile_page.html', user=current_user, pharmacies=pharmacies, pharmacount=pharmacount)
+
+
+@app.route('/donate', methods=["POST", "GET"])
+@login_required
 def donate():
-    form=medical_equipment_form()
+    form = medical_equipment_form()
     if form.validate_on_submit():
         existing_request = donation_box.query.filter_by(
             equip_name=form.equip_name.data,
@@ -35,17 +45,17 @@ def donate():
         if existing_request:
             flash("Request already exists", "danger")
             return render_template('donate.html', form=form)
-        
-        donation = donation_box (
+
+        donation = donation_box(
             equip_name=form.equip_name.data,
             userid=current_user.userid,
             pincode=current_user.pincode
-            )
+        )
         db.session.add(donation)
         db.session.commit()
 
-        equip_image=request.files['equip_image']
-        bill_image=request.files['bill_image']
+        equip_image = request.files['equip_image']
+        bill_image = request.files['bill_image']
         donation_id = donation.id
         img_ext1 = os.path.splitext(equip_image.filename)[1]
         if bill_image:
@@ -53,34 +63,34 @@ def donate():
             img_name2 = f"{donation_id}{img_ext2}"
             bill_image_path = os.path.join(BILL_UPLOAD_FOLDER, img_name2)
             bill_image.save(bill_image_path)
-        img_name1 =  f"{donation_id}{img_ext1}"
+        img_name1 = f"{donation_id}{img_ext1}"
         equip_image_path = os.path.join(EQU_UPLOAD_FOLDER, img_name1)
         equip_image.save(equip_image_path)
         flash("This is done", "success")
-    return render_template('donate.html', form=form,user=current_user)
+    return render_template('donate.html', form=form, user=current_user)
+
 
 @app.route('/pharma_home')
 def pharma_home():
     print(current_user.get_id())
-    all_request=medicine_request_pool.query.filter_by(pharmaid=current_user.get_id())
+    all_request = medicine_request_pool.query.filter_by(
+        pharmaid=current_user.get_id())
     print(MED_UPLOAD_FOLDER)
-    return render_template('pharma_home.html', current_user=current_user,all_request=all_request,MED_UPLOAD_FOLDER=MED_UPLOAD_FOLDER, PRE_UPLOAD_FOLDER= PRE_UPLOAD_FOLDER)
+    return render_template('pharma_home.html', current_user=current_user, all_request=all_request, MED_UPLOAD_FOLDER=MED_UPLOAD_FOLDER, PRE_UPLOAD_FOLDER=PRE_UPLOAD_FOLDER)
+
 
 @app.route('/pharma_user')
 def pharma_user():
     print(current_user.get_id())
-    all_request=medicine_request_pool.query.filter_by(pharmaid=current_user.get_id())
-    return render_template('pharma_home.html', current_user=current_user,all_request=all_request)
+    all_request = medicine_request_pool.query.filter_by(
+        pharmaid=current_user.get_id())
+    return render_template('pharma_home.html', current_user=current_user, all_request=all_request)
 
-@app.route('/profile_page')
-def profile_page():
-    pharmacies=pharmacy.query.filter_by(pincode=current_user.pincode)
-    pharmacount=pharmacy.query.filter_by(pincode=current_user.pincode).count()
-    return render_template('profile_page.html', user=current_user,pharmacies=pharmacies,pharmacount=pharmacount)
 
 @app.route('/test')
 def test():
     return f"Current user: {current_user.username}"
+
 
 @app.route('/logout')
 def logout():
@@ -88,38 +98,39 @@ def logout():
     flash('You have been logged out.', 'success')
     return redirect(url_for('home'))
 
-@app.route("/add_points",methods=["POST"])
+
+@app.route("/add_points", methods=["POST"])
 def add_points():
     if request.method == 'POST':
-        request_id=request.form.get("request_id")
-        request_points=request.form.get("user_points")
-        current_request=medicine_request_pool.query.get(request_id)
-        current_request.points_given=request_points
+        request_id = request.form.get("request_id")
+        request_points = request.form.get("user_points")
+        current_request = medicine_request_pool.query.get(request_id)
+        current_request.points_given = request_points
         redirect_url = request.form.get('redirect_url')
         db.session.commit()
     return redirect(redirect_url)
 
 
-
-@app.route("/reject",methods=["POST"])
+@app.route("/reject", methods=["POST"])
 def reject():
     if request.method == 'POST':
-        request_id=request.form.get("request_id")
-        request_reason=request.form.get("rejection_reason")
-        current_request=medicine_request_pool.query.get(request_id)
+        request_id = request.form.get("request_id")
+        request_reason = request.form.get("rejection_reason")
+        current_request = medicine_request_pool.query.get(request_id)
         redirect_url = request.form.get('redirect_url')
-        current_request.is_rejected=1
-        current_request.reason=request_reason
+        current_request.is_rejected = 1
+        current_request.reason = request_reason
         db.session.commit()
     return redirect(redirect_url)
 
-@app.route("/approve",methods=["POST"])
+
+@app.route("/approve", methods=["POST"])
 def approve():
     if request.method == 'POST':
-        request_id=request.form.get("request_id")
-        current_request=medicine_request_pool.query.get(request_id)
+        request_id = request.form.get("request_id")
+        current_request = medicine_request_pool.query.get(request_id)
         redirect_url = request.form.get('redirect_url')
-        current_request.is_approved=1
+        current_request.is_approved = 1
         db.session.commit()
     return redirect(redirect_url)
 
@@ -158,8 +169,9 @@ def login_users():
         user = users.query.filter_by(email=email).first()
         if user:
             if user.check_password(password):
-                login_user(user)                
-                flash(f'Account created successfully! You are now logged in as {user.username} ', category='success')
+                login_user(user)
+                flash(f'Account created successfully! You are now logged in as {
+                      user.username} ', category='success')
                 return redirect(url_for("home"))
             else:
                 flash("Wrong password", "danger")
@@ -172,10 +184,10 @@ def login_users():
 @app.route('/med_return', methods=['GET', 'POST'])
 @login_required
 def med_return():
-    if current_user.pharmaid==0:
+    if current_user.pharmaid == 0:
         flash("Please select default pharmacy", "danger")
         return redirect(url_for("profile_page"))
-    form = medicine_return_form()    
+    form = medicine_return_form()
     pharmacies = pharmacy.query.filter_by(pincode=current_user.pincode).all()
     if form.validate_on_submit():
         # Check if the request already exists
@@ -188,7 +200,7 @@ def med_return():
         if existing_request:
             flash("Request already exists", "danger")
             return render_template('med_return.html', form=form)
-        
+
         # Extract multiple medications from form and join them into a single string
         medications_str = ', '.join(request.form.getlist('meds'))
 
@@ -202,12 +214,12 @@ def med_return():
         )
         db.session.add(requests)
         db.session.commit()
-        
+
         medicine_image = request.files['medicine_image']
         prescription_image = request.files['prescription_image']
         img_extension1 = os.path.splitext(medicine_image.filename)[1]
         img_extension2 = os.path.splitext(prescription_image.filename)[1]
-        
+
         request_id = requests.id
         img_name1 = f"{request_id}{img_extension1}"
         img_name2 = f"{request_id}{img_extension2}"
@@ -231,11 +243,12 @@ def register_pharma():
             password=form2.password.data,
             pincode=form2.pincode.data,
             address=form2.address.data
-            )
+        )
         user = pharmacy.query.filter_by(email=create_user.email).first()
         if user:
             flash("account already exists", "general")
-            return redirect(url_for("login_pharma"))  # Correct the redirect URL
+            # Correct the redirect URL
+            return redirect(url_for("login_pharma"))
         db.session.add(create_user)
         db.session.commit()
         login_user(create_user)
@@ -254,8 +267,9 @@ def login_pharma():
         user = pharmacy.query.filter_by(email=email).first()
         if user:
             if user.check_password(password):
-                login_user(user)                
-                flash(f'Pharamcy Account created successfully! You are now logged in as {user.username} ', category='success')
+                login_user(user)
+                flash(f'Pharamcy Account created successfully! You are now logged in as {
+                      user.username} ', category='success')
                 return redirect(url_for("pharma_home"))
             else:
                 flash("Wrong password", "danger")
@@ -265,6 +279,8 @@ def login_pharma():
     return render_template('login_pharma.html', login_form=form1, register_form=form2)
 
 # medicine scheduler
+
+
 @app.route('/med_scheduler', methods=['GET', 'POST'])
 @login_required
 def med_scheduler():
@@ -281,39 +297,42 @@ def med_scheduler():
             notes=form.notes.data
         )
         # Check if the medicine already exists for the current user
-        med = meds_scheduler.query.filter_by(user_id=current_user.userid, med_name=form.med_name.data).first()
+        med = meds_scheduler.query.filter_by(
+            user_id=current_user.userid, med_name=form.med_name.data).first()
         if med:
             flash("Medicine already exists", "danger")
-            return redirect(url_for('med_scheduler'))  # Redirect instead of rendering the template
+            # Redirect instead of rendering the template
+            return redirect(url_for('med_scheduler'))
         db.session.add(schedule)
         db.session.commit()
         flash('Medicine scheduled successfully', 'success')
-        return redirect(url_for('med_scheduler'))  # Redirect to avoid resubmission on page refresh
+        # Redirect to avoid resubmission on page refresh
+        return redirect(url_for('med_scheduler'))
     else:
         print("Form errors:", form.errors)  # Debugging print for form errors
     return render_template('med_scheduler.html', form=form)
 
 
-
-@app.route("/change_pincode",methods=["POST"])
+@app.route("/change_pincode", methods=["POST"])
 def change_pincode():
     if request.method == 'POST':
-        pincode=request.form.get("new_pincode")
-        current_user.pincode=pincode
+        pincode = request.form.get("new_pincode")
+        current_user.pincode = pincode
         redirect_url = request.form.get('redirect_url')
         db.session.commit()
     return redirect(redirect_url)
 
-@app.route("/select_pharma",methods=["POST","GET"])
+
+@app.route("/select_pharma", methods=["POST", "GET"])
 def select_pharma():
     if request.method == 'POST':
-        id=request.form.get("selected_pharmacy")
-        phar=pharmacy.query.get(id)
-        address=phar.address
-        name=phar.username
-        current_user.pharmaid=id
-        current_user.address=address
-        current_user.pharmacy_name=name
+        id = request.form.get("selected_pharmacy")
+        phar = pharmacy.query.get(id)
+        address = phar.address
+        name = phar.username
+        current_user.pharmaid = id
+        current_user.address = address
+        current_user.pharmacy_name = name
         redirect_url = request.form.get('redirect_url')
         db.session.commit()
     return redirect(redirect_url)
